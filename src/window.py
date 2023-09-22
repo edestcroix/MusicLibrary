28181504b7864bc8689f05b5409ad5ccd6cddb5d
Emmett de St. Croix <emmett@edestcroix.ca>
2023-09-21 17:32:33 -0700

    Basic ui prototyping

--- src/window.py
+++ src/window.py
@@ -20,23 +20,37 @@
 from gi.repository import Adw
 from gi.repository import Gtk
 
+import mutagen
+
 
 @Gtk.Template(resource_path='/ca/edestcroix/MusicLibary/window.ui')
 class MusiclibraryWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'MusicLibraryWindow'
 
-    # artist_view = Gtk.Template.Child()
-    # album_view = Gtk.Template.Child()
+    artist_box = Gtk.Template.Child()
+    album_box = Gtk.Template.Child()
+
+    test_button = Gtk.Template.Child()
+
+    c = 0
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        # artist_box is a GtkListBox
+
         # artist_model = Gtk.ListStore(str)
         # album_model = Gtk.ListStore(str)
 
-        # for i in range(100):
-        #     artist_model.append([f'Artist {i}'])
-        #     album_model.append([f'Album {i}'])
+        # bind test_button to test_button_clicked
+        self.test_button.connect('clicked', self.test_button_clicked)
+
+        for i in range(100):
+            # self.artist_box.append(Gtk.Label(label=f'Artist {i}'))
+            self.album_box.append(Gtk.Label(label=f'Album {i}'))
+
+        # bind clicks on elements of artist_box to artist_clicked
+        self.artist_box.connect('row-activated', self.select_artist)
 
         # renderer = Gtk.CellRendererText()
 
@@ -50,3 +64,16 @@ class MusiclibraryWindow(Adw.ApplicationWindow):
         # self.artist_view.append_column(artist_column)
         # self.album_view.set_model(album_model)
         # self.album_view.append_column(album_column)
+
+    def test_button_clicked(self, _):
+        print('test button clicked')
+
+        self.artist_box.append(Gtk.Label(label=f'test ({self.c})'))
+        self.c += 1
+
+    # TODO: This will need to update the album list to show
+    # only albums by the selected artist.
+    def select_artist(self, _, clicked_row):
+        if clicked_row:
+            print(clicked_row.get_index())
+            print(clicked_row.get_child().get_label())
