1bc705d71b9bb5d02c81c31ee616ad89a76beb97
Emmett de St. Croix <emmett@edestcroix.ca>
2023-10-31 22:37:23 -0700

    Improve seek detection
    While somehow the previous version didn't enter a seek loop when a track changed, it seemed like there
    were occational micro-seeks on track changes which caused stutters. Now a seek can only happen if the
    saved duration value matches the one obtained from the player, so seeks can't trigger during track changes.

--- src/monitor.py
+++ src/monitor.py
@@ -30,20 +30,23 @@ class ProgressMonitor:
         return False
 
     def _update_state(self):
-        if self._need_seek():
-            value = self._scale.get_value()
-            self._last_position = value
-            self._player.seek(value * TIME_DIVISOR)
-            return True
-
-        progress, duration = self._get_player_state()
+        position, duration = self._get_player_state()
+        # if durations don't match, the track has changed.
         if duration != self._duration:
             # depending on when the duration is queried, the pipeline might return -1 if it
             # hasn't finished loading the track yet, so the abs() prevents range errors.
             self._scale.set_range(0, abs(duration))
             self._duration = duration
-        self._update_widgets(progress)
-        self._last_position = progress
+            self._last_position = 0
+            self._update_widgets(0)
+        else:
+            # otherwise, check for a seek
+            value = self._scale.get_value()
+            if abs(value - self._last_position) > 1:
+                self._player.seek(value * TIME_DIVISOR)
+            else:
+                self._update_widgets(position)
+            self._last_position = value
         return True
 
     def _get_player_state(self):
@@ -51,14 +54,6 @@ class ProgressMonitor:
             self._player.get_progress() / TIME_DIVISOR,
             self._player.get_duration() / TIME_DIVISOR,
         )
-
-    # check if the player needs to seek to the current position
-    # This is done by checking if the difference between the current position
-    # and the last, rather than connecting to the scale's change-value signal
-    # because that signal sends really inconsistent and sometimes negative values,
-    # which causes the player to seek back to the start of the track.
-    def _need_seek(self):
-        return abs(self._last_position - self._scale.get_value()) > 1
 
     def _update_widgets(self, value):
         progress = self._format_time(value)
